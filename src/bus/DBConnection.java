b5a61f42ef2d283439ff06564a7a651a15ce832e
Chrispine Chiedo <chrispinechiedo@gmail.com>
2020-07-18 14:07:28 +0300

    update DBConnection file

--- src/bus/DBConnection.java
+++ src/bus/DBConnection.java
@@ -12,14 +12,14 @@ public class DBConnection {
     public static Connection getDBConnection() throws SQLException {
         Connection connection = null;
 
-        // Note that using a plaintext password like in this case isn't a good practice!
-        // Did this for local testing ONLY! It is highly recommended that a hash of the
-        // password is used instead, especially in production code.
         try {
-            connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost/bus?useSSL=false",
-                        "root",
-                        "chiedo7saleh");
+
+            String url = "jdbc:mysql://localhost/bus?useSSL=false";
+            String user = "root";
+            String password = "chiedo7saleh";
+
+            connection = DriverManager.getConnection(url, user, password);
+
             return connection;
         } catch (SQLException e) {
             System.err.println(e);
